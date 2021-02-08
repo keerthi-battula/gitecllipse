9179f8ecc2482c024f24dade6f1ae94166d4cf52
keerthi-battula <net2bks@gmail.com>
2021-02-08 11:58:05 +0530

    first commit

---
+++ gitecllipse/src/gitecllipse/GitFun.java
@@ -0,0 +1,16 @@
+package gitecllipse;
+public class GitFun {
+	public static void main(String args[])
+	{
+		int a=10;
+		int b=14;
+		int c=a+b;
+		System.out.println("the sum of a and b is:");
+		System.out.println(c);
+		System.out.println("thank you!");
+		System.out.println("please visit again!");
+		System.out.println("have a great day!");
+		System.out.println("first change");
+
+	}
+}
